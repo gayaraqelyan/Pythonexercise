d502e33f863d7163facde673ed7b3c06c6f57f13
gayaraqelyan <116947014+gayaraqelyan@users.noreply.github.com>
2024-11-15 23:20:43 +0400

    Add files via upload

---
+++ For-loop.py
@@ -0,0 +1,15 @@
+# __________________________________________________Exercise  1 -------------------------------------------------------
+# num = int(input("Enter the number  "))
+# #Find the factorial of a given number
+# factorial=1
+# for i in range(1,num+1):
+#    factorial=factorial*i
+# print("The factorial this is number ",factorial)
+
+# import math
+# print(math.factorial(5))
+#
+# Write a program to print multiplication table of a given number
+# num = int(input("enter the number  "))
+# for i in range(1,11):
+#     print(i ,'*',num ,'=',i*num)
