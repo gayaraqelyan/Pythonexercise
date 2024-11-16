37ec55d6b1b25ebc1772200038b83479a0a6be3d
gayaraqelyan <116947014+gayaraqelyan@users.noreply.github.com>
2024-11-16 13:26:56 +0400

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,44 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# Write a Python function to sum all the numbers in a list.
+# def sumList(item):
+#    sum = 0
+#    for i in item:
+#        sum = sum + i
+#    return sum
+
+
+# print(sumList([8, 5, 3]))
+
+# -----------------------------------------------------------------------------------------------------------------------
+# Write a Python function to check whether a number is in a given range.
+
+# a = 10
+# b = 9
+# c = 20
+
+# def numbers_range(a, b, c):
+# return b <= a <= c
+
+# if numbers_range(a, b, c):
+#    print(f"The a number {a}is in the range {b}to {c}.")
+# else:
+#    print(f"The a number {a}is not in the range {b}to {c}.")
+
+# Write a Python function that accepts a string and calculate the number of upper case letters and lower case letters.
+# Simple string:'The quick Brown Fox'
+# def count_case_letters(input_string):
+# upper_case_count = 0
+# lower_case_count = 0
+
+# for char in input_string:
+#     if char.isupper():
+#         upper_case_count += 1
+#     elif char.islower():
+#         lower_case_count += 1
+# return {"Uppercase": upper_case_count, "Lowercase": lower_case_count}
+
+# simple_string = 'The quick Brown Fox'
+# result = count_case_letters(simple_string)
+# print(f"Original String:'{simple_string}'")
+# print(f"Number uppercase letters:{result['Uppercase']}")
+# print(f"Number lowercase letters:{result['Lowercase']}")
