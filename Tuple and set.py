60c70231efa92376ee25321c4f9355187cc84ecf
gayaraqelyan <116947014+gayaraqelyan@users.noreply.github.com>
2024-11-16 13:32:01 +0400

    Add files via upload

---
+++ Tuple and set.py
@@ -0,0 +1,18 @@
+# ------------------------------------------Exercise 1-------------------------------------
+# Check i a value 200 exists in dictionary:
+# sampleDic = {
+#   "a": 100,
+#  "b": 200,
+#  "c": 300}
+#  for i in sampleDic.values():
+#  if i == 200:
+#  print("Yes")
+
+
+# -------------------------------------------Exercise 2 -------------------------------------
+# Change Brad's salary into 8500 in the given dictionary.
+# sampleDec = {
+#    "emp1": {"name": "John", "salary": 7000},
+#   "emp2": {"name": "Brad", "salary": 6500}}
+#   sampleDec["emp2"]["salary"] = 8500
+#   print(sampleDec)
